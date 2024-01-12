fc5788daf32145f36d732776cb86ce579d1fd288
baxnul <khafizovilnaz100@gmail.com>
2024-01-12 19:07:00 +0300

    Добавлено: 1) Перейти на https://sbis.ru/ в раздел "Контакты" 2) Проверить, что определился ваш регион (в нашем примере Ярославская обл.) и есть список партнеров. 3) Изменить регион на Камчатский край 4) Проверить, что подставился выбранный регион.
    Также вынесены общие методы из sbis_base_page.py и tensor_base_page.py в файл base_page.py

---
+++ pages/base_page.py
@@ -0,0 +1,101 @@
+from selenium.common.exceptions import *
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasePage:
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        # self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        """Открыть ссылку"""
+        self.browser.get(self.url)
+
+    def is_element_present(self, by, locator, timeout=4) -> bool:
+        """Проверка на присутсвие элемента на странице"""
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).until(
+                EC.presence_of_element_located((by, locator)))
+        except TimeoutException:
+            return False
+        return True
+
+    def get_element(self, by, locator, timeout=10) -> object:
+        """Найти и получить элемент для дальнейшего взаимодействия с ним"""
+        try:
+            element = WebDriverWait(self.browser, timeout).until(
+                EC.presence_of_element_located((by, locator))
+            )
+            return element
+        except (TimeoutException, NoSuchElementException) as e:
+            print(f"Element with locator {locator} not found: {e}")
+            return None
+
+    def get_elements(self, by, locator, timeout=10):
+        """Найти и получить список элементов для дальнейшего взаимодействия с ними"""
+        try:
+            elements = WebDriverWait(self.browser, timeout).until(
+                EC.presence_of_all_elements_located((by, locator))
+            )
+            return elements
+        except (TimeoutException, NoSuchElementException) as e:
+            print(f"Elements with locator locator not found: e")
+            return []
+
+    def element_to_be_clickable(self, by, locator, timeout=10) -> object:
+        """Ждать пока элемент не станет кликабельным, получаем элемент для дальнейшего взаимодействия с ним"""
+        try:
+            element = WebDriverWait(self.browser, timeout).until(
+                EC.element_to_be_clickable((by, locator))
+            )
+            return element
+        except (TimeoutException, NoSuchElementException) as e:
+            print(f"Element with locator {locator} not found: {e}")
+            return None
+
+    def visibility_of_element_located(self, by, locator, timeout=10) -> object:
+        """Ждать пока элемент не станет видимый,
+           получить элемент для дальнейшего взаимодействия с ним"""
+        try:
+            element = WebDriverWait(self.browser, timeout).until(
+                EC.visibility_of_element_located((by, locator))
+            )
+            return element
+        except (TimeoutException, NoSuchElementException) as e:
+            print(f"Element with locator locator not found: e")
+            return None
+
+    def wait_for_text_in_element(self, by, locator, text_, timeout=10) -> bool:
+        """Ждать появления текста в элементе,
+            на вход принимает локатор и текст"""
+        try:
+            WebDriverWait(self.browser, timeout).until(
+                EC.text_to_be_present_in_element((by, locator), text_)
+            )
+            return True
+        except TimeoutException:
+            return False
+
+    def is_not_element_present(self, by, locator, timeout=4) -> bool:
+        """Элемент не появляется на странице в течение заданного времени.
+        Упадет, как только увидит искомый элемент. Не появился: успех, тест зеленый"""
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((by, locator)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, by, locator, timeout=4) -> bool:
+        """Проверить, что какой-то элемент исчезает.
+         Будет ждать до тех пор, пока элемент не исчезнет."""
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(
+                EC.presence_of_element_located((by, locator)))
+        except TimeoutException:
+            return False
+        return True
+
+    def switch_to_last_window(self):
+        return self.browser.switch_to.window(self.browser.window_handles[-1])  # Переключиться на последнюю вкладку
